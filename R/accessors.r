218e4666a2baa4c3eaf4221e43a170ef99e2cffd
garrettgman <grolemund@gmail.com>
2009-05-26 16:28:10 -0500

    Realized previous changes were unnecessary :P Undid them.

--- R/accessors.r
+++ R/accessors.r
@@ -1,31 +1,32 @@
 # Extract components from a date
 # -----------------------------------------------------------
 
-# Converts to a recognizable date format to POSIXlt list format, then extract 
+# Converts a recognizable date format to POSIXlt list format, then extract 
 #   desired information.
 # Note: The origin refers to the origin time in GMT
-second <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$sec
-minute <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$min
-hour <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$hour
-yday <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$yday + 1
-wday <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$wday + 1
-mday <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$mday
-week <- function(x, origin = "1970-01-01") yday(x, origin = origin) %/% 7 + 1
-month <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$mon + 1
-year <- function(x, origin = "1970-01-01") as.POSIXlt(x, origin = origin)$year + 1900
+# Note: Not meant to apply to non-date objects.  These should first be converted to POSIXct with parse.r
+second <- function(x) as.POSIXlt(x)$sec
+minute <- function(x) as.POSIXlt(x)$min
+hour <- function(x) as.POSIXlt(x)$hour
+yday <- function(x) as.POSIXlt(x)$yday + 1
+wday <- function(x) as.POSIXlt(x)$wday + 1
+mday <- function(x) as.POSIXlt(x)$mday
+week <- function(x) yday(x) %/% 7 + 1
+month <- function(x) as.POSIXlt(x)$mon + 1
+year <- function(x) as.POSIXlt(x)$year + 1900
 
 # Extract the last entry in the time zone vector.
 # Note: Should this be the first entry? It appears that the last entry will 
 #   sometimes be the base timezone and sometimes the daylight savings alternate 
 #   time zone. On the otherhand, the first entry is often "".
-tz <- function(x, origin = "1970-01-01") {
-  tzs <- attr(as.POSIXlt(x, origin = origin),"tzone")
+tz <- function(x) {
+  tzs <- attr(as.POSIXlt(x),"tzone")
   tzs[length(tzs)]
 }
 
 # am is true iff the time is before 12:00 noon, otherwise pm is true 
-am <- function(x, origin = "1970-01-01") hour(x, origin) < 12
-pm <- function(x, origin = "1970-01-01") !am(x, origin)
+am <- function(x) hour(x) < 12
+pm <- function(x) !am(x)
 
 
 # Set components of a date, modifying the value in place
@@ -37,34 +38,34 @@ pm <- function(x, origin = "1970-01-01") !am(x, origin)
 
 #Note: If present, fractional seconds will remain. Do we want to discard these 
 #  when we set a value? Maybe just when we set the seconds value?
-"second<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (second(x, origin) - value)
-"minute<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (minute(x, origin) - value) * 60
-"hour<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (hour(x, origin) - value) * 3600
-"yday<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (yday(x, origin) - value) * 3600 * 24
-"wday<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (wday(x, origin) - value) * 3600 * 24
-"mday<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (mday(x, origin) - value) * 3600 * 24
-"week<-" <- function(x, value, origin = "1970-01-01") as.POSIXlt(x, origin = origin) - (week(x, origin) - value) * 3600 * 24 * 7
+"second<-" <- function(x, value) as.POSIXlt(x) - (second(x) - value)
+"minute<-" <- function(x, value) as.POSIXlt(x) - (minute(x) - value) * 60
+"hour<-" <- function(x, value) as.POSIXlt(x) - (hour(x) - value) * 3600
+"yday<-" <- function(x, value) as.POSIXlt(x) - (yday(x) - value) * 3600 * 24
+"wday<-" <- function(x, value) as.POSIXlt(x) - (wday(x) - value) * 3600 * 24
+"mday<-" <- function(x, value) as.POSIXlt(x) - (mday(x) - value) * 3600 * 24
+"week<-" <- function(x, value) as.POSIXlt(x) - (week(x) - value) * 3600 * 24 * 7
 
 # Create a new POSIXlt date using the components of our old date with a modified 
 #    month value.  A new month value > 12, affects both the year and month values.
 
-"month<-" <- function(x, value, origin = "1970-01-01") {
+"month<-" <- function(x, value) {
   ISOdatetime(
-    year(x, origin) + (value - 1) %/% 12,  
+    year(x) + (value - 1) %/% 12,  
     
     # %% is the modulo operator in R
     (value - 1) %% 12 + 1, 
-    mday(x, origin), 
-    hour(x, origin), 
-    minute(x, origin), 
-    second(x, origin), 
-    tz(x, origin))
+    mday(x), 
+    hour(x), 
+    minute(x), 
+    second(x), 
+    tz(x))
 }
-"year<-" <- function(x, value, origin = "1970-01-01") {
-  ISOdatetime(value,  month(x, origin), mday(x, origin), hour(x, origin), minute(x, origin), second(x, origin), tz(x, origin))
+"year<-" <- function(x, value) {
+  ISOdatetime(value,  month(x), mday(x), hour(x), minute(x), second(x), tz(x))
 }
-"tz<-" <- function(x, value, origin = "1970-01-01") {
-  ISOdatetime(year(x, origin),  month(x, origin), mday(x, origin), hour(x, origin), minute(x, origin), second(x, origin),
+"tz<-" <- function(x, value) {
+  ISOdatetime(year(x),  month(x), mday(x), hour(x), minute(x), second(x),
     value)
 }
 
@@ -78,7 +79,7 @@ pm <- function(x, origin = "1970-01-01") !am(x, origin)
 #   be in the set (second, minute, hour, yday, wday, mday, week, month, year, 
 #   tz). 
 # Perhaps we should add an error message to clarify this?
-update.Date <- update.POSIXt <- function(object, , origin = "1970-01-01", ...) {
+update.Date <- update.POSIXt <- function(object, ...) {
 
   # a list of the changes we wish to make
   changes <- list(...)
@@ -89,7 +90,7 @@ update.Date <- update.POSIXt <- function(object, , origin = "1970-01-01", ...) {
     f <- match.fun(paste(change, "<-", sep = ""))
     
     # we update the date with the new change
-    object <- f(object, changes[[change]], origin)
+    object <- f(object, changes[[change]])
   }
   object
 }
