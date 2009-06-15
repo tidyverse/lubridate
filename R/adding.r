100a56e9e8a827426aba985e38475239854857a8
garrettgman <grolemund@gmail.com>
2009-06-15 12:56:04 -0500

    Complete adding function

--- R/adding.r
+++ R/adding.r
@@ -53,5 +53,15 @@ m <- months(1)
 y <- years(1)
 	
 # creating new adding function
-
+"+.Date" <- function(e1, e2){
+	if inherits(e2, "duration"){
+		if (e2 %% floor(e2) == 0){ #whole periods
+			seq(e1, by = "month", length.out = e2)[e2]
+			} else{ # partial periods
+				e1 + floor(e2)) + e2 %% floor(e2) * ((e1 + floor(e2)) - (e1 + ceiling(e2)))
+				}
+		} else {
+			base::'+.Date'(e1, e2)
+			}
+	}
 
