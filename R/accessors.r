f7004035a9d438d62923e9e95175e946ae3eb3a4
hadley <h.wickham@gmail.com>
2009-08-20 21:12:25 -0500

    Break out pretty intervals into own file.  Move am and pm to utils

--- R/accessors.r
+++ R/accessors.r
@@ -530,20 +530,6 @@ dst.fts <- function(x)
 dst.irts <- function(x)
   dst.default(x$time)
 
-#' Does date time occur in the am or pm?
-#'
-#' @aliases am pm
-#' @param x a date-time object  
-#' @return TRUE or FALSE depending on whethe x occurs in the am or pm 
-#' @keywords chron 
-#' @examples
-#' x <- now()
-#' am(x) 
-#' pm(x) 
-am <- function(x) hour(x) < 12
-pm <- function(x) !am(x)
-
-
 #' Internal function. Replaces the seconds element of a date with a specified 
 #' value.
 #' 
