c5a59105be1008c769a4c149ba956fcaf93328fc
garrettgman <grolemund@gmail.com>
2010-05-05 12:00:32 -0500

    wrote tests for intervals.r

---
+++ tests/interval-tests.R
@@ -0,0 +1,53 @@
+context("intervals tests")
+
+test_that("new_interval works as expected", {
+	int <- new_interval(as.POSIXct("2008-08-03 13:01:59"), 
+		as.POSIXct("2009-08-03 13:01:59"))
+	test.list <- structure(list(start = as.POSIXct("2008-08-03 
+		13:01:59"), end = as.POSIXct("2009-08-03 13:01:59")),
+		class = c("interval", "data.frame"))
+		
+	expect_that(int, equals(test.list))
+		
+})
+
+test_that("new_interval handles vector input", {
+	a <- as.POSIXct("2008-08-03 13:01:59")
+	b <- as.POSIXct("2009-08-03 13:01:59")
+	d <- as.POSIXct("2009-08-04 13:01:59")
+	
+	int <- new_interval(c(a,b),d) 
+	test.list <- structure(data.frame(start = c(a,b), end = d), class = c("interval", "data.frame"))
+		
+	expect_that(int, equals(test.list))
+})
+
+
+test_that("print_interval works as expected", {
+	int <- new_interval(as.POSIXct("2008-08-03 13:01:59"), 
+		as.POSIXct("2009-08-03 13:01:59"))
+	expect_that(print(int), 
+		matches("365 days beginning at 2008-08-03 13:01:59"))
+})
+
+
+test_that("as_interval works as expected", {
+	a <- as.POSIXct("2008-08-03 13:01:59")
+	
+	expect_that(as.interval(days(1), a), equals(new_interval(a, 
+		a + days(1))))
+})
+
+test_that("as_interval handles vector input", {
+		a <- as.POSIXct("2008-08-03 13:01:59")
+		b <- as.POSIXct("2009-08-03 13:01:59")
+	
+	expect_that(as.interval(days(1:2), a), equals(new_interval(a, 
+		a + days(1:2))))
+	expect_that(as.interval(days(1:2), c(a,b)), 
+		equals(new_interval(c(a,b), c(a,b) + days(1:2))))
+	expect_that(as.interval(days(1), c(a,b)), 
+		equals(new_interval(c(a,b), c(a,b) + days(1))))
+	expect_that(as.interval(c(days(1), minutes(1)), a), 
+		equals(new_interval(a, a + c(days(1), minutes(1)))))
+})
