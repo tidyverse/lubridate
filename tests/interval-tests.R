a2a07bada59c7083f8a34eb91b18afb1c50a83cc
garrettgman <grolemund@gmail.com>
2010-05-05 13:22:42 -0500

    wrote tests for periods.r

--- tests/interval-tests.R
+++ tests/interval-tests.R
@@ -8,6 +8,7 @@ test_that("new_interval works as expected", {
 		class = c("interval", "data.frame"))
 		
 	expect_that(int, equals(test.list))
+	expect_that(class(int)[1], matches("interval"))
 		
 })
 
@@ -23,7 +24,7 @@ test_that("new_interval handles vector input", {
 })
 
 
-test_that("print_interval works as expected", {
+test_that("print.interval works as expected", {
 	int <- new_interval(as.POSIXct("2008-08-03 13:01:59"), 
 		as.POSIXct("2009-08-03 13:01:59"))
 	expect_that(print(int), 
@@ -31,14 +32,16 @@ test_that("print_interval works as expected", {
 })
 
 
-test_that("as_interval works as expected", {
+test_that("as.interval works as expected", {
 	a <- as.POSIXct("2008-08-03 13:01:59")
 	
 	expect_that(as.interval(days(1), a), equals(new_interval(a, 
 		a + days(1))))
+	expect_that(as.interval(edays(1), a), equals(new_interval(a, 
+		a + edays(1))))
 })
 
-test_that("as_interval handles vector input", {
+test_that("as.interval handles vector input", {
 		a <- as.POSIXct("2008-08-03 13:01:59")
 		b <- as.POSIXct("2009-08-03 13:01:59")
 	
