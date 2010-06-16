8fb1cfb5e53c684c5c6affff692fa23acb67687a
hadley <h.wickham@gmail.com>
2010-06-16 17:37:48 -0500

    Minimal coercion documentation

--- R/coercion.r
+++ R/coercion.r
@@ -1,10 +1,7 @@
-# Generic accessor code for date time classes we work with
-#
-# See tests/accessor.R for test code
-
-# install.packages(c("chron", "timeDate", "zoo", "xts", "its", "tis", 
-#  "timeSeries", "fts", "tseries"))
-
+#' Convert every date method we know about to POSIXlt and POSIXct
+#' @name DateCoercion
+#' @keywords internal 
+#' @aliases as.POSIXlt.fts as.POSIXlt.its as.POSIXlt.timeSeries as.POSIXlt.irts as.POSIXlt.xts as.POSIXlt.zoo as.POSIXlt.tis as.POSIXct.fts as.POSIXct.its as.POSIXct.timeSeries as.POSIXct.irts as.POSIXct.xts as.POSIXct.zoo as.POSIXct.tis
 as.POSIXlt.fts <- function(x, tz = "", ...) as.POSIXlt(fts::dates.fts(x))
 as.POSIXct.fts <- function(x, tz = "", ...) as.POSIXct(fts::dates.fts(x))
 
@@ -30,7 +27,9 @@ as.POSIXct.zoo <- function(x, tz = "", ...) as.POSIXct(zoo::index(x))
 
 as.POSIXlt.tis <- function(x, tz = "", ...) as.Date(x)
 
-
+#' Convenience method to reclass dates post-modification.
+#' @keywords internal
+#' @aliases reclass_date reclass_date.POSIXlt reclass_date.POSIXct reclass_date.chron reclass_date.timeDate reclass_date.its reclass_date.ti reclass_date.Date
 reclass_date <- function(new, orig) UseMethod("reclass_date", orig)
 reclass_date.POSIXlt <- function(new, orig) {
   as.POSIXlt(new)
