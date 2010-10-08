4c0f101d1bfb65452fe2f4ff8cda977a620f8c99
Garrett <grolemund@gmail.com>
2010-10-08 11:59:41 -0500

    created subsecond accessor and settor functions

--- R/accessors-second.r
+++ R/accessors-second.r
@@ -23,3 +23,28 @@ second.default <- function(x)
 
 "second<-" <- function(x, value)
   x <- x + seconds(value - second(x))
+  
+  
+picosecond <- function(x)
+	(second(x) - floor(second(x))) / 10^(-12)
+	
+"picosecond<-" <- function(x, value)
+	x <- x + picoseconds(value - picosecond(x))
+	
+nanosecond <- function(x)
+	(second(x) - floor(second(x))) / 10^(-9)
+	
+"nanosecond<-" <- function(x, value)
+	x <- x + nanoseconds(value - nanosecond(x))
+	
+microsecond <- function(x)
+	(second(x) - floor(second(x))) / 10^(-6)
+	
+"microsecond<-" <- function(x, value)
+	x <- x + microseconds(value - microsecond(x))
+	
+millisecond <- function(x)
+	(second(x) - floor(second(x))) / 10^(-3)
+	
+"millisecond<-" <- function(x, value)
+	x <- x + milliseconds(value - millisecond(x))
