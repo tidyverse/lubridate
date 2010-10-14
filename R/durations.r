d206c6cdd27438036eb11e91c440d3dfdcf9c3e5
Garrett <grolemund@gmail.com>
2010-10-14 11:11:59 -0500

    created new data structure for durations

--- R/durations.r
+++ R/durations.r
@@ -4,19 +4,22 @@
 #' period. 
 #'
 #' Durations record the exact number of seconds in a time span. They measure the 
-#' exact passage of time and are not affected by conventions such as leap years 
+#' exact passage of time but do not always align with measurements 
+#' made in larger units of time such as hours, months and years. 
+#' This is because the length of larger time units can be affected 
+#' by conventions such as leap years 
 #' and Daylight Savings Time. Base R measures durations with the 
-#' difftime class. lubridate provides additional difftime methods 
-#' to facilitate working with this class. 
+#' difftime class. lubridate provides an additional class, the duration class,
+#' to facilitate working with durations. 
 #'
-#' difftime displays durations in various units, but these units are estimates 
+#' durations display as the number of seconds that occur during a time span. If the number is large, a duration object will also display the length in a more convenient unit, but these measurements are only estimates 
 #' given for convenience. The underlying object is always recorded as a fixed 
 #' number of seconds. For display and creation purposes, units are converted to 
 #' seconds using their most common lengths in seconds. Minutes = 60 seconds, 
-#' hours = 3600 seconds, days = 86400 seconds, weeks = 604800. Units larger than 
-#' weeks are not used due to their variability.
+#' hours = 3600 seconds, days = 86400 seconds. Units larger than 
+#' days are not used due to their variability.
 #' 
-#' difftime (i.e duration) objects can be easily created with the helper functions 
+#' duration objects can be easily created with the helper functions 
 #' \code{\link{dweeks}}, \code{\link{ddays}}, \code{\link{dhours}}, \code{\link{dminutes}} and 
 #' \code{\link{dseconds}}. These objects can be added to and subtracted from date-
 #' times to create a user interface similar to object oriented programming. 
@@ -47,8 +50,9 @@
 #' time during the interval can be calculated. To 
 #' accurately convert between periods and durations, a period or duration should 
 #' first be converted to an interval with \code{\link{as.interval}}. Subtracting two date times automatically 
-#' creates an interval object. Intervals display as the difftime between the two 
-#' dates. 
+#' creates an interval object. An interval displays as the start 
+#' and end points of the time span it represents. By default, the #' date-time that occurs first is considered the start point. 
+#' Hence, intervals are always positive.
 #'
 #' @aliases duration durations dur periods period interval intervals timespans
 #' @name duration
@@ -59,13 +63,13 @@
 #' @keywords classes chron
 #' @examples
 #' new_duration(second = 3690)
-#' # Time difference of 1.025 hours
+#' # 3690s (1.02h)
 #' new_period(second = 3690)
 #' # 3690 seconds
 #' new_period(second = 30, minute = 1, hour = 1)
 #' # 1 hour, 1 minute and 30 seconds
 #' new_interval(ymd_hms("2009-08-09 13:01:30"), ymd_hms("2009-08-09 12:00:00"))
-#' # [1] 1.025 hours beginning at 2009-08-09 12:00:00
+#' # 2009-08-09 12:00:00 -- 2009-08-09 13:01:30
 #'
 #' date <- as.POSIXct("2009-03-08 01:59:59") # DST boundary
 #' # "2009-03-08 01:59:59 CST"
@@ -89,11 +93,11 @@
 #' #[11] "2009-11-30 01:00:00 CST" "2009-12-31 01:00:00 CST"
 #'
 #' span <- date2 - date  #creates interval 
-#' # 3294.583 days beginning at 2000-02-29 12:00:00 
+#' # 2000-02-29 12:00:00 -- 2009-03-08 01:59:59
 #' span - days(294)
-#' # 3000.542 days beginning at 2000-02-29 12:00:00
+#' # 2000-02-29 12:00:00 -- 2008-05-18 01:59:59
 #' span - ddays(294)
-#' # 3000.583 days beginning at 2000-02-29 12:00:00
+#' # 2000-02-29 12:00:00 -- 2008-05-18 02:59:59
 #'
 #' date <- as.POSIXct("2009-01-01 00:00:00") 
 #' # "2009-01-01 GMT"
@@ -110,7 +114,66 @@ NULL
 
 #' Create a duration object.
 #'
-#' new_duration creates a difftime object with the specified values. Entries for 
+#' new_duration creates a duration object with the specified values. Entries for 
+#' different units are cumulative. durations display as the number of seconds in a time span. When this number is large, durations also display an estimate in larger units,; however, the underlying object is 
+#' always recorded as a fixed number of seconds. For display and creation 
+#' purposes, units are converted to seconds using their most common lengths in 
+#' seconds. Minutes = 60 seconds, hours = 3600 seconds, days = 86400 seconds, 
+#' weeks = 604800. Units larger than weeks are not used due to their 
+#' variability.
+#'
+#' Durations record the exact number of seconds in a time span. They measure the 
+#' exact passage of time but do not always align with measurements 
+#' made in larger units of time such as hours, months and years. 
+#' This is because the length of larger time units can be affected 
+#' by conventions such as leap years 
+#' and Daylight Savings Time. Base R provides a second class for measuring durations, the difftime class.
+#'
+#' duration objects can be easily created with the helper functions 
+#' \code{\link{dweeks}}, \code{\link{ddays}}, \code{\link{dminutes}}, 
+#' \code{\link{dseconds}}. These objects can be added to and subtracted to date-
+#' times to create a user interface similar to object oriented programming. 
+#'
+#' @param ... a list of time units to be included in the duration and their amounts. Seconds, 
+#'   minutes, hours, days, and weeks are supported.
+#' @return a duration object
+#' @aliases new_duration
+#' @seealso \code{\link{duration}}, \code{\link{as.duration}}
+#' @keywords chron classes
+#' @examples
+#' new_duration(second = 90)
+#' # 90s
+#' new_duration(minute = 1.5)
+#' # 90s
+#' new_duration(second = 3, minute = 1.5, hour = 2, day = 6, week = 1)
+#' # 1130493s (13.08d)
+#' new_duration(hour = 1, minute = -60)
+#' # 0s
+#' new_duration(day = -1)
+#' # -86400s (-1d)
+new_duration <- function(num = 0,...){
+  pieces <- list(...)
+  names(pieces) <- standardise_difftime_names(names(pieces))
+  
+  defaults <- list(secs = 0, mins = 0, hours = 0, days = 0, weeks = 0)
+  pieces <- c(pieces, defaults[setdiff(names(defaults), names(pieces))])
+  
+  x <- num + pieces$secs +
+    pieces$mins * 60 +
+    pieces$hours * 3600 +
+    pieces$days * 86400 +
+    pieces$weeks * 604800
+  
+  structure(x, class = c("duration", "numeric"))
+}
+
+
+
+
+
+#' Create a difftime object.
+#'
+#' new_difftime creates a difftime object with the specified number of units. Entries for 
 #' different units are cumulative. difftime displays durations in various units, 
 #' but these units are estimates given for convenience. The underlying object is 
 #' always recorded as a fixed number of seconds. For display and creation 
@@ -119,33 +182,31 @@ NULL
 #' weeks = 604800. Units larger than weeks are not used due to their 
 #' variability.
 #'
-#' difftime objects are durations. Durations record the exact number of seconds 
-#' in a time span. They measure the exact passage of time and are not affected 
-#' by conventions such as leap years and Daylight Savings Time. 
+#' difftime objects are durations. Durations are time spans measured in seconds. They measure the 
+#' exact passage of time but do not always align with measurements 
+#' made in larger units of time such as hours, months and years. 
+#' This is because the length of larger time units can be affected 
+#' by conventions such as leap years 
+#' and Daylight Savings Time. lubridate provides a second class for measuring durations, the duration class.
 #'
-#' duration objects can be easily created with the helper functions 
-#' \code{\link{eweeks}}, \code{\link{edays}}, \code{\link{eminutes}}, 
-#' \code{\link{eseconds}}. These objects can be added to and subtracted to date-
-#' times to create a user interface similar to object oriented programming. 
-#'
-#' @param ... a list of time units to be included in the duration and their amounts. Seconds, 
-#'   minutes, hours, days, and weeks are supported. See \code{\link{standardise_difftime_names}}.
-#' @return a duration object
+#' @param ... a list of time units to be included in the difftime and their amounts. Seconds, 
+#'   minutes, hours, days, and weeks are supported. 
+#' @return a difftime object
 #' @aliases new_duration new_difftime
 #' @seealso \code{\link{duration}}, \code{\link{as.duration}}
 #' @keywords chron classes
 #' @examples
-#' new_duration(second = 90)
+#' new_difftime(second = 90)
 #' # Time difference of 1.5 mins
-#' new_duration(minute = 1.5)
+#' new_difftime(minute = 1.5)
 #' # Time difference of 1.5 mins
-#' new_duration(second = 3, minute = 1.5, hour = 2, day = 6, week = 1)
+#' new_difftime(second = 3, minute = 1.5, hour = 2, day = 6, week = 1)
 #' # Time difference of 1.869201 weeks
-#' new_duration(hour = 1, minute = -60)
+#' new_difftime(hour = 1, minute = -60)
 #' # Time difference of 0 secs
-#' new_duration(day = -1)
+#' new_difftime(day = -1)
 #' # Time difference of -1 days
-new_duration <- new_difftime <- function(...){
+new_difftime <- function(...){
   pieces <- list(...)
   names(pieces) <- standardise_difftime_names(names(pieces))
   
@@ -163,11 +224,11 @@ new_duration <- new_difftime <- function(...){
 
 #' Change an object to a duration (difftime).
 #'
-#' as.duration changes interval, period and numeric objects to duration objects, 
-#' which are difftime objects. Numeric objects are changed to duration objects 
+#' as.duration changes interval, period and numeric objects to 
+#' duration objects. Numeric objects are changed to duration objects 
 #' with the seconds unit equal to the numeric value. 
 #'
-#' Durations are exact time measurements, but periods are relative time 
+#' Durations are exact time measurements, whereas periods are relative time 
 #' measurements. See \code{\link{periods}}. The length of a period depends on 
 #' when it occurs. Hence, a one to one mapping does not exist between durations 
 #' and periods. When used with a period object, as.duration provides an inexact 
@@ -183,12 +244,12 @@ new_duration <- new_difftime <- function(...){
 #' @seealso \code{\link{duration}}, \code{\link{new_duration}}
 #' @keywords classes manip methods chron
 #' @examples
-#' span <- new_interval(as.POSIXct("2009-01-01"), as.POSIXct("2009-08-01")) #interval
-#' # 211.9583 days beginning at 2009-01-01
+#' span <- new_interval(ymd("2009-01-01"), ymd("2009-08-01")) #interval
+#' # 2009-01-01 -- 2009-08-01 
 #' as.duration(span)
-#' # Time difference of 211.9583 days
+#' # 18316800s (212d)
 #' as.duration(10) # numeric
-#' # Time difference of 10 secs
+#' # 10s
 as.duration <- function(x)
   UseMethod("as.duration")
   
@@ -202,20 +263,18 @@ as.duration.period <- function(x){
     x$day * 3600 * 24 + 
     x$year * 3600 * 24 * 365
   
-  make_difftime(all)
+  structure(all, class = c("duration", "numeric"))
 }
 
 as.duration.difftime <- function(x)
-  make_difftime(as.numeric(x, "secs"))
+  structure(as.numeric(x, "secs"), class = c("duration", "numeric"))
   
 as.duration.interval <- function(x){
-  attr(x, "start") <- NULL
-  class(x) <- c("difftime")
-  x
+  structure(unclass(x), class = c("duration", "numeric"))
 }
 
 as.duration.default <- function(x)
-  make_difftime(x)
+  structure(x, class = c("duration", "numeric"))
 
 #' Internal function.
 #'
@@ -240,38 +299,37 @@ as.POSIXt <- function(x) as.POSIXlt(x)
 #' @seealso \code{\link{duration}}, \code{\link{new_duration}}, \code{\link{days}}
 #' @keywords chron manip
 #' @examples
-#' eseconds(1) 
-#' # Time difference of 1 secs
-#' eminutes(3.5) 
-#' # Time difference of 3.5 mins
+#' dseconds(1) 
+#' # 1s
+#' dminutes(3.5) 
+#' # 210s
 #'
 #' x <- as.POSIXct("2009-08-03") 
 #' # "2009-08-03 CDT"
-#' x + edays(1) + ehours(6) + eminutes(30)
+#' x + ddays(1) + dhours(6) + dminutes(30)
 #' # "2009-08-04 06:30:00 CDT"
-#' x + edays(100) - ehours(8) 
+#' x + ddays(100) - dhours(8) 
 #' # "2009-11-10 15:00:00 CST"
 #' 
-#' class(as.Date("2009-08-09") + edays(1)) # retains Date class
+#' class(as.Date("2009-08-09") + ddays(1)) # retains Date class
 #' # "Date"
-#' as.Date("2009-08-09") + ehours(12) 
+#' as.Date("2009-08-09") + dhours(12) 
 #' # "2009-08-09 12:00:00 UTC"
-#' class(as.Date("2009-08-09") + ehours(12)) 
+#' class(as.Date("2009-08-09") + dhours(12)) 
 #' # "POSIXt"  "POSIXct"
 #' # converts to POSIXt class to accomodate time units
 #' 
-#' eweeks(1) - edays(7) 
-#' # Time difference of 0 secs
-#' c(1:3) * ehours(1) 
-#' # Time differences in hours
-#' # [1] 1 2 3
+#' dweeks(1) - ddays(7) 
+#' # 0s
+#' c(1:3) * dhours(1) 
+#' # 3600s  7200s  10800s
 #' #
 #' # compare DST handling to durations
 #' boundary <- as.POSIXct("2009-03-08 01:59:59")
 #' # "2009-03-08 01:59:59 CST"
 #' boundary + days(1) # period
 #' # "2009-03-09 01:59:59 CDT" (clock time advances by a day)
-#' boundary + edays(1) # duration
+#' boundary + ddays(1) # duration
 #' # "2009-03-09 02:59:59 CDT" (clock time corresponding to 86400 seconds later)
 dseconds <- eseconds <- function(x = 1) new_duration(second = x)
 dminutes <- eminutes <- function(x = 1) new_duration(minute = x)
@@ -300,13 +358,13 @@ is.instant <- is.timepoint <- function(x) inherits(x, c("POSIXt", "POSIXct", "PO
 #'
 #' @aliases is.timespan timespan
 #' @param x an R object   
-#' @return TRUE if x is a period, interval, or difftime object, FALSE otherwise.
+#' @return TRUE if x is a period, interval, duration, or difftime object, FALSE otherwise.
 #' @seealso \code{\link{is.instant}}, \code{\link{is.duration}}, \code{\link{is.difftime}}, \code{\link{is.period}}, \code{\link{is.interval}}
 #' @keywords logic chron
 #' @examples
 #' is.timespan(as.Date("2009-08-03")) # FALSE
 #' is.timespan(new_duration(second = 1)) # TRUE
-is.timespan <- function(x) inherits(x,c("period", "difftime", "interval"))
+is.timespan <- function(x) inherits(x,c("period", "difftime", "duration", "interval"))
 
 #' Is x a POSIXct or POSIXlt object?
 #'
@@ -322,18 +380,29 @@ is.POSIXt <- function(x) inherits(x, c("POSIXt", "POSIXct", "POSIXlt"))
 is.POSIXlt <- function(x) inherits(x, "POSIXlt")
 is.POSIXct <- function(x) inherits(x, "POSIXct")
 
-#' Is x a duration (difftime) object?
+#' Is x a duration object?
 #'
-#' @aliases is.difftime is.duration
 #' @param x an R object   
-#' @return TRUE if x is a difftime object, FALSE otherwise.
+#' @return TRUE if x is a duration object, FALSE otherwise.
 #' @seealso \code{\link{is.instant}}, \code{\link{is.timespan}}, \code{\link{is.interval}}, 
 #'   \code{\link{is.period}}, \code{\link{duration}}
 #' @keywords logic chron
 #' @examples
+#' is.duration(as.Date("2009-08-03")) # FALSE
+#' is.duration(new_duration(days = 12.4)) # TRUE
+is.duration <- function(x) inherits(x, "duration")
+
+#' Is x a difftime object?
+#'
+#' @param x an R object   
+#' @return TRUE if x is a difftime object, FALSE otherwise.
+#' @seealso \code{\link{is.instant}}, \code{\link{is.timespan}}, \code{\link{is.interval}}, 
+#'   \code{\link{is.period}}.
+#' @keywords logic chron
+#' @examples
 #' is.difftime(as.Date("2009-08-03")) # FALSE
-#' is.difftime(new_duration(days = 12.4)) # TRUE
-is.difftime <- is.duration <- function(x) inherits(x, "difftime")
+#' is.difftime(new_difftime(days = 12.4)) # TRUE
+is.difftime <- function(x) inherits(x, "difftime")
 
 #' Is x a Date object?
 #'
@@ -390,4 +459,18 @@ standardise_difftime_names <- function(x) {
   res
 }
 
+
+format.duration <- function(x, ...){
+  if (all(abs(unclass(x)) < 3600))
+  	return(paste(format(unclass(x),...), "s", sep = ""))
+  else
+  	simple <- make_difftime(unclass(x))
+  	num <- round(as.numeric(simple), 2)
+  	units <- substr(attr(simple, "units"),1,1)
+  	paste(format(unclass(x),...), "s", " (", num, units, ") ", sep = "") 
+}
+
+print.duration <- function(x, ...) {
+  print(format(x), ..., quote = FALSE)
+}
 
