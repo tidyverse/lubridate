80021406c600eb111233db1073c7547317536286
Garrett <grolemund@gmail.com>
2010-10-20 12:09:55 -0500

    updated tests for durations

--- inst/tests/test-durations.R
+++ inst/tests/test-durations.R
@@ -1,16 +1,16 @@
 context("Durations")
 
-test_that("new_duration works as expected",{
+test_that("new_difftime works as expected",{
   x <- as.POSIXct("2008-08-03 13:01:59", tz = "UTC")
   y <- difftime(x + 5 + 30*60 + 60*60 + 14*24*60*60, x, tz = "UTC")
   attr(y, "tzone") <- NULL
-  diff <- new_duration(seconds = 5, minutes = 30, days = 0, 
+  diff <- new_difftime(seconds = 5, minutes = 30, days = 0, 
     hour = 1, weeks = 2)
   
   expect_that(diff, equals(y))
 })
 
-test_that("new_duration handles vectors",{
+test_that("new_difftime handles vectors",{
   x <- as.POSIXct(c("2008-08-03 13:01:59", "2008-08-03 13:01:59"), tz = "UTC")
   y <- difftime(x + c(5 + 30*60 + 60*60 + 14*24*60*60, 
     1 + 3*24*60*60 + 60*60), x, tz = "UTC")
@@ -20,12 +20,35 @@ test_that("new_duration handles vectors",{
   attr(z, "tzone") <- NULL
     
   
-  expect_that(new_duration(seconds = c(5, 1), minutes = c(30,
+  expect_that(new_difftime(seconds = c(5, 1), minutes = c(30,
     0), days = c(0, 3), hour = c(1,1), weeks = c(2, 0)),
     equals(y))
     
-  expect_that(new_duration(seconds = 5, minutes = 30, days = 
+  expect_that(new_difftime(seconds = 5, minutes = 30, days = 
     c(0, 3), hour = 1, weeks = 2), equals(z))
+
+})
+
+
+test_that("new_duration works as expected",{
+  x <- structure(1215005, class = c("duration", "numeric"))
+  dur <- new_duration(seconds = 5, minutes = 30, days = 0, 
+    hour = 1, weeks = 2)
+  
+  expect_that(dur, equals(x))
+})
+
+
+test_that("new_duration handles vectors",{
+  dur1 <- new_duration(seconds = c(5, 1), minutes = c(30, 0), 
+  	days = c(0, 3), hour = c(1,1), weeks = c(2, 0))
+  dur2 <- new_duration(seconds = 5, minutes = 30, days = 
+    c(0, 3), hour = 1, weeks = 2)
+  y <- structure(c(1215005, 262801), class = c("duration", "numeric"))
+  z <- structure(c(1215005, 1474205), class = c("duration", "numeric"))  
+  
+  expect_that(dur1, equals(y))
+  expect_that(dur2, equals(z))
 
 })
 
@@ -37,41 +60,38 @@ test_that("as.duration handles vectors",{
 
 test_that("as.duration handles periods",{
   
-  expect_that(as.duration(seconds(1)), equals(eseconds(1)))
-  expect_that(as.duration(minutes(2)), equals(eminutes(2)))
-  expect_that(as.duration(hours(3)), equals(ehours(3)))
-  expect_that(as.duration(days(4)), equals(edays(4)))
-  expect_that(as.duration(weeks(5)), equals(eweeks(5)))
-  expect_that(as.duration(years(1)), equals(eseconds(60*60*24*365)))
-  expect_that(as.duration(seconds(1) + minutes(4)), equals(eseconds(1) + eminutes(4)))
-})
-
-test_that("as.duration gives error for months",{
-  expect_that(as.duration(months(1)), throws_error("durations cannot estimate month length"))
+  expect_that(as.duration(seconds(1)), equals(dseconds(1)))
+  expect_that(as.duration(minutes(2)), equals(dminutes(2)))
+  expect_that(as.duration(hours(3)), equals(dhours(3)))
+  expect_that(as.duration(days(4)), equals(ddays(4)))
+  expect_that(as.duration(weeks(5)), equals(dweeks(5)))
+  expect_that(as.duration(months(1)), equals(dseconds(60*60*24*30)))
+  expect_that(as.duration(years(1)), equals(dseconds(60*60*24*365.25)))
+  expect_that(as.duration(seconds(1) + minutes(4)), equals(dseconds(1) + dminutes(4)))
 })
 
 test_that("as.duration handles intervals",{
 	time1 <- as.POSIXct("2009-01-02 12:24:03", tz = "UTC")
 	time2 <- as.POSIXct("2010-02-03 14:31:42", tz = "UTC")
 	int <- new_interval(time2, time1)
-    y <- difftime(time2, time1)
+    y <- as.numeric(difftime(time2, time1), units = "secs")
+    z <- structure(y, start = time1, class = c("duration", "numeric"))
   
-  expect_that(as.duration(int), equals(y))
+  expect_that(as.duration(int), equals(z))
 })
 
 test_that("as.duration handles difftimes",{
   x <- difftime(as.POSIXct("2010-02-03 14:31:42", tz = "UTC"),
     as.POSIXct("2009-01-02 12:24:03", tz = "UTC"))
-  attr(x, "tzone") <- NULL
+  y <- structure(as.numeric(x, units = "secs"), class = c("duration", "numeric"))
   
-  expect_that(as.duration(x), equals(x))
+  expect_that(as.duration(x), equals(y))
 })
 
 
 test_that("eobjects handle vectors",{
-  x <- as.POSIXct("2008-08-03 13:01:59", tz = "UTC")
-  expect_that(eseconds(c(1,3,4)), equals(difftime(x + c(1,3,4),
-    x)))
+  x <-structure(c(1,3,4), class = c("duration", "numeric"))
+  expect_that(dseconds(c(1,3,4)), equals(x))
 })
 
 
@@ -135,23 +155,40 @@ test_that("is.POSIXt handles vectors",{
 })
 
 
-test_that("is.difftime/is.duration works as expected",{
+test_that("is.difftime works as expected",{
   ct_time <- as.POSIXct("2008-08-03 13:01:59", tz = "UTC")
   lt_time <- as.POSIXlt("2009-08-03 13:01:59", tz = "UTC")
 	
   expect_that(is.difftime(234), is_false())
-  expect_that(is.difftime(ct_time),
-    is_false())
-  expect_that(is.difftime(lt_time), 
-    is_false())
+  expect_that(is.difftime(ct_time), is_false())
+  expect_that(is.difftime(lt_time), is_false())
   expect_that(is.difftime(Sys.Date()), is_false())
   expect_that(is.difftime(minutes(1)), is_false())
-  expect_that(is.difftime(eminutes(1)), is_true())
-  expect_that(is.difftime(new_interval(lt_time, ct_time)), is_true())
+  expect_that(is.difftime(eminutes(1)), is_false())
+  expect_that(is.difftime(new_difftime(1000)), is_true())
+  expect_that(is.difftime(new_interval(lt_time, ct_time)), is_false())
 })
 
-test_that("is.difftime/is.duration handle vectors",{
-  expect_that(is.difftime(eminutes(1:3)), is_true())
+test_that("is.duration works as expected",{
+  ct_time <- as.POSIXct("2008-08-03 13:01:59", tz = "UTC")
+  lt_time <- as.POSIXlt("2009-08-03 13:01:59", tz = "UTC")
+	
+  expect_that(is.duration(234), is_false())
+  expect_that(is.duration(ct_time), is_false())
+  expect_that(is.duration(lt_time), is_false())
+  expect_that(is.duration(Sys.Date()), is_false())
+  expect_that(is.duration(minutes(1)), is_false())
+  expect_that(is.duration(eminutes(1)), is_true())
+  expect_that(is.duration(new_difftime(1000)), is_false())
+  expect_that(is.duration(new_interval(lt_time, ct_time)), is_false())
+})
+
+test_that("is.difftime handle vectors",{
+  expect_that(is.difftime(new_difftime(1:3)), is_true())
+})
+
+test_that("is.duration handle vectors",{
+  expect_that(is.duration(dminutes(1:3)), is_true())
 })
 
 
