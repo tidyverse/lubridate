eb9f6994d77c245ea3fc946bce7e8849f39fd16f
Garrett <grolemund@gmail.com>
2011-01-04 13:38:57 -0600

    created month.integer method

--- R/periods.r
+++ R/periods.r
@@ -88,16 +88,13 @@ new_period <- function(...) {
 #' @export seconds minutes hours days weeks years y m w d milliseconds microseconds microseconds nanoseconds picoseconds
 #' @aliases seconds minutes hours days weeks years y m w d milliseconds microseconds microseconds nanoseconds picoseconds
 #' @S3method months numeric
+#' @S3method months integer
 #' @param x numeric value of the number of units to be contained in the period. With the exception 
 #'   of seconds(), x must be an integer. 
 #' @return a period object
-#' @seealso \code{\link{period}}, \code{\link{new_period}}, \code{\link{edays}}
+#' @seealso \code{\link{period}}, \code{\link{new_period}}, \code{\link{ddays}}
 #' @keywords chron manip
 #' @examples
-#' eseconds(1) 
-#' # Time difference of 1 secs
-#' eminutes(3.5) 
-#' # Time difference of 3.5 mins
 #'
 #' x <- as.POSIXct("2009-08-03") 
 #' # "2009-08-03 CDT"
@@ -147,7 +144,7 @@ minutes <- function(x = 1) new_period(minute = x)
 hours <-   function(x = 1) new_period(hour = x)
 days <-    function(x = 1) new_period(day = x)  
 weeks <-   function(x = 1) new_period(week = x)
-months.numeric <-  function(x) new_period(month = x)
+months.numeric <- months.integer <- function(x, abbreviate) new_period(month = x)
 years <-   function(x = 1) new_period(year = x)
 y <- years(1)
 m <- months(1)
@@ -157,6 +154,7 @@ milliseconds <- function(x = 1) seconds(x/1000)
 microseconds <- function(x = 1) seconds(x/1000000)
 nanoseconds <- function(x = 1) seconds(x/1e9)
 picoseconds <- function(x = 1) seconds(x/1e12)
+
 
 
 format.period <- function(x, ...){
