174934ff80f8460eb94ae80b5dc710beb337a0d3
garrettgman <grolemund@gmail.com>
2012-02-06 11:50:24 -0600

    Added roxygen documentation and export commands to all files. Documented package using devtools.

--- R/ops-subtraction.r
+++ R/ops-subtraction.r
@@ -16,41 +16,42 @@ subtract_interval_from_date <- function(int, date){
 	add_duration_to_date(-dur, date)
 }
 
-#' Subtracting date-time objects
-#'
-#' subtract_dates subtracts two objects, both of which can be date-time 
-#' objects. Lubridate is built so that you don't need to use 
-#' subtract_dates but can use regular math expressions instead.
-#' @param e1 a numeric or date-time object
-#' @param e2 a numeric or date-time object
-#' @export subtract_dates
 
+#' @export
 setMethod("-", signature(e1 = "Duration", e2 = "missing"),
     function(e1, e2) multiply_duration_by_number(e1, -1))
-    
+
+#' @export    
 setMethod("-", signature(e1 = "Interval", e2 = "missing"),
 	function(e1, e2) multiply_interval_by_number(e1, -1))
-    
+
+#' @export    
 setMethod("-", signature(e1 = "Period", e2 = "missing"),
     function(e1, e2) multiply_period_by_number(e1, -1))
 
 
 
+#' @export
 setMethod("-", signature(e2 = "Duration"), 
 	function(e1, e2) e1  + (-1 * e2))
 
+#' @export
 setMethod("-", signature(e1 = "Interval", e2 = "Interval"),
 	function(e1, e2) setdiff(e1, e2))
-	
+
+#' @export	
 setMethod("-", signature(e2 = "Period"), 
 	function(e1, e2) e1  + (-1 * e2))
 
+#' @export
 setMethod("-", signature(e1 = "Date", e2 = "Interval"),
 	function(e1, e2) subtract_interval_from_date(e2, e1))
 
+#' @export
 setMethod("-", signature(e1 = "POSIXct", e2 = "Interval"),
 	function(e1, e2) subtract_interval_from_date(e2, e1))
-	
+
+#' @export	
 setMethod("-", signature(e1 = "POSIXlt", e2 = "Interval"),
 	function(e1, e2) subtract_interval_from_date(e2, e1))
 	
