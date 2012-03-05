706a218c19ba7d6dfc888425b80376ad90341ad8
garrettgman <grolemund@gmail.com>
2012-03-05 09:08:23 -0600

    changed unit to units in difftime call to avoid warning message.

--- R/round.r
+++ R/round.r
@@ -128,7 +128,7 @@ round_date <- function(x, unit = c("second","minute","hour","day", "week", "mont
   below <- as.POSIXct(floor_date(x, unit))
   above <- as.POSIXct(ceiling_date(x, unit))
 
-  smaller <- difftime(as.POSIXct(x), below, unit = "secs") < difftime(above, as.POSIXct(x), unit = "secs")
+  smaller <- difftime(as.POSIXct(x), below, units = "secs") < difftime(above, as.POSIXct(x), units = "secs")
   new <- structure(ifelse(smaller, below, above), class = class(below))
   
   attr(new, "tzone") <- tz(x)
