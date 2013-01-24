856b6934ee007723bff50e25d6f1d95ede973b3c
Vitalie Spinu <spinuvit@gmail.com>
2013-01-25 00:49:56 +0100

    remove test for helper_offset

--- inst/tests/test-parsers.R
+++ inst/tests/test-parsers.R
@@ -365,25 +365,6 @@ test_that("ymd_hms parses OO and Oo formats correctly", {
               equals(ymd_hms(c("2012-03-04 04:06:07", "2012-03-04 03:36:07"), tz="UTC")))  
 })
 
-test_that("helper_offset parses correctly", {
-  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="Europe/Paris")), 
-              equals("+01:00"))
-  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York")), 
-              equals("-05:00"))
-  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York"), sep=":"), 
-              equals("-05:00"))
-  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York"), sep=""), 
-              equals("-0500"))
-  ## change through daylight-saving time change
-  expect_that(helper_offset(ymd_hms("2012-03-11T01:00:00", tz="America/New_York"), sep=""), 
-              equals("-0500"))
-  expect_that(helper_offset(ymd_hms("2012-03-11T03:00:00", tz="America/New_York"), sep=""), 
-              equals("-0400"))
-  ## change through daylight-saving time + vector
-  expect_that(helper_offset(ymd_hms(c("2012-03-11T01:00:00", "2012-03-11T03:00:00"), 
-                                    tz="America/New_York"), sep=""), 
-              equals(c("-0500", "-0400")))  
-})
 
 
 
