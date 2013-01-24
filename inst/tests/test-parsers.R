9f4828fbc501733261a2d5b1890402aebc8403dd
Vitalie Spinu <spinuvit@gmail.com>
2013-01-25 00:01:18 +0100

    Move Ian Lyttle's test into test-parsers.R and modfy to use ymd_hms instead of ymd_hms_o.

--- inst/tests/test-parsers.R
+++ inst/tests/test-parsers.R
@@ -332,6 +332,61 @@ test_that("ISO8601: xxx_hms functions work correctly with z, Ou, OO and Oo forma
 })
 
 
+
+test_that("ymd_hms parses Ou format correctly ",{
+  expect_that(ymd_hms("2012-03-04T05:06:07Z"), 
+              equals(ymd_hms("2012-03-04 05:06:07", tz="UTC")))
+})
+
+test_that("ymd_hms parses OO and Oo formats correctly", {
+  ## +00:00
+  expect_that(ymd_hms("2012-03-04T05:06:07+00:00"), 
+              equals(ymd_hms("2012-03-04 05:06:07", tz="UTC")))
+  ## -HH
+  expect_that(ymd_hms("2012-03-04T05:06:07-01"), 
+              equals(ymd_hms("2012-03-04 06:06:07", tz="UTC")))
+  ## -HHMM
+  expect_that(ymd_hms("2012-03-04T05:06:07-0130"), 
+              equals(ymd_hms("2012-03-04 06:36:07", tz="UTC")))
+  ## -HH:MM
+  expect_that(ymd_hms("2012-03-04T05:06:07-01:30"), 
+              equals(ymd_hms("2012-03-04 06:36:07", tz="UTC")))
+  ## +HH
+  expect_that(ymd_hms("2012-03-04T05:06:07+01"), 
+              equals(ymd_hms("2012-03-04 04:06:07", tz="UTC")))
+  ## +HHMM
+  expect_that(ymd_hms("2012-03-04T05:06:07+0130"), 
+              equals(ymd_hms("2012-03-04 03:36:07", tz="UTC")))
+  ## +HH:MM
+  expect_that(ymd_hms("2012-03-04T05:06:07+01:30"), 
+              equals(ymd_hms("2012-03-04 03:36:07", tz="UTC")))
+  ## vectorizes
+  expect_that(ymd_hms(c("2012-03-04T05:06:07+01", "2012-03-04T05:06:07+01:30")), 
+              equals(ymd_hms(c("2012-03-04 04:06:07", "2012-03-04 03:36:07"), tz="UTC")))  
+})
+
+test_that("helper_offset parses correctly", {
+  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="Europe/Paris")), 
+              equals("+01:00"))
+  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York")), 
+              equals("-05:00"))
+  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York"), sep=":"), 
+              equals("-05:00"))
+  expect_that(helper_offset(ymd_hms("2012-03-04T05:06:07", tz="America/New_York"), sep=""), 
+              equals("-0500"))
+  ## change through daylight-saving time change
+  expect_that(helper_offset(ymd_hms("2012-03-11T01:00:00", tz="America/New_York"), sep=""), 
+              equals("-0500"))
+  expect_that(helper_offset(ymd_hms("2012-03-11T03:00:00", tz="America/New_York"), sep=""), 
+              equals("-0400"))
+  ## change through daylight-saving time + vector
+  expect_that(helper_offset(ymd_hms(c("2012-03-11T01:00:00", "2012-03-11T03:00:00"), 
+                                    tz="America/New_York"), sep=""), 
+              equals(c("-0500", "-0400")))  
+})
+
+
+
 ## c("2012-12-04 15:06:06.952000-08:00", "2012-12-04 15:04:01.640000-08:00", 
 ##   "2012-12-02 17:58:31.141000-08:00", "2012-12-04 17:15:14.091000-08:00", 
 ##   "2012-12-04 17:16:05.097000-08:00")
