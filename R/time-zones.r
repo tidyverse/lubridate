d5c422ff2f9c0bd48134adcf96708e88a9531fbf
Garrett <grolemund@gmail.com>
2013-02-14 09:23:01 -0600

    Rewrote force_tz. New version is 30x faster. Fixes #161.

--- R/time-zones.r
+++ R/time-zones.r
@@ -2,11 +2,14 @@
 #' 
 #' with_tz returns a date-time as it would appear in a different time zone.  
 #' The actual moment of time measured does not change, just the time zone it is 
-#' measured in. with_tz defaults to the Universal Coordinated time zone (UTC) when an unrecognized time zone is inputted. See \code{\link{Sys.timezone}} for more information on how R recognizes time zones.
+#' measured in. with_tz defaults to the Universal Coordinated time zone (UTC) 
+#' when an unrecognized time zone is inputted. See \code{\link{Sys.timezone}} 
+#' for more information on how R recognizes time zones.
 #'
 #' @export with_tz
 #' @param time a POSIXct, POSIXlt, Date, or chron date-time object.
-#' @param tzone a character string containing the time zone to convert to. R must recognize the name 
+#' @param tzone a character string containing the time zone to convert to. R 
+#' must recognize the name 
 #'   contained in the string as a time zone on your system.
 #' @return a POSIXct object in the updated time zone
 #' @keywords chron manip
@@ -16,6 +19,7 @@
 #' with_tz(x, "GMT")
 #' # "2009-08-07 04:00:01 GMT"
 with_tz <- function (time, tzone = ""){
+  check_tz(tzone)
   if (is.POSIXlt(time)) new <- as.POSIXct(time)
   else new <- time
   attr(new, "tzone") <- tzone
@@ -24,15 +28,20 @@ with_tz <- function (time, tzone = ""){
 
 #' Replace time zone to create new date-time
 #' 
-#' force_tz returns a the date-time that has the same clock time as x in the new time zone.  
+#' force_tz returns a the date-time that has the same clock time as x in the new
+#'  time zone.  
 #' Although the new date-time has the same clock time (e.g. the 
 #' same values in the year, month, days, etc. elements) it is a 
-#' different moment of time than the input date-time. force_tz defaults to the Universal Coordinated time zone (UTC) when an unrecognized time zone is inputted. See \code{\link{Sys.timezone}} for more information on how R recognizes time zones.
+#' different moment of time than the input date-time. force_tz defaults to the 
+#' Universal Coordinated time zone (UTC) when an unrecognized time zone is 
+#' inputted. See \code{\link{Sys.timezone}} for more information on how R 
+#' recognizes time zones.
 #'
 #' @export force_tz
 #'
 #' @param time a POSIXct, POSIXlt, Date, or chron date-time object.
-#' @param tzone a character string containing the time zone to convert to. R must recognize the name 
+#' @param tzone a character string containing the time zone to convert to. R 
+#' must recognize the name 
 #' contained in the string as a time zone on your system.
 #' @return a POSIXct object in the updated time zone
 #' @keywords chron manip
@@ -42,16 +51,22 @@ with_tz <- function (time, tzone = ""){
 #' force_tz(x, "GMT")
 #' # "2009-08-07 00:00:01 GMT"
 force_tz <- function(time, tzone = ""){  
-  if(is.null(tzone)) tzone <- ""
-  new <- ISOdatetime(year(time), month(time), mday(time), hour(time),
-    minute(time), second(time), tzone)
+  check_tz(tzone)
+  lt <- as.POSIXlt(time)
+  attr(lt, "tzone") <- tzone
   
-  missing_hour <- hour(with_tz(new, tzone)) != hour(time)
-  new[missing_hour] <- NA
-    
-  reclass_date(new, time)
+  ct <- as.POSIXct(lt)
+  
+  # check if date falls in DST gap
+  lt.hours <- lt$hour %% 24 + lt$min %/% 60 + lt$sec %/% 3600
+  if (any(na.omit(lt.hours < 3)) && tzone != "UTC") { 
+    ct[!is.na(ct) & hour(ct) != lt.hours] <- NA
+  }
+  
+  reclass_date(ct, time)
 }
 
+check_tz <- function(tz) {}
 
 # Note: alternative method? as.POSIXlt(format(as.POSIXct(x)), tz = tz)
 
