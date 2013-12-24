92270540521a7ccffc95ad922e78b1ec3bddad6f
Vitalie Spinu <spinuvit@gmail.com>
2013-12-23 23:05:44 -0800

    rewrite parse_dt to allow for formats (strptime) and orders (parse_date_time)

--- src/tparse.c
+++ src/tparse.c
@@ -15,17 +15,20 @@ static const long long yearlen = 31536000; // common year in sec: 365*24*60*60
 
 /* quick way to check if the first char is a digit */
 #define DIGIT(X) ((X) >= '0' && (X) <= '9')
-#define PARSENUM(X) while (DIGIT(*c)) { X = X * 10 + (*c - '0'); c++; }
+#define PARSENUM(X, N) D=N; while ( DIGIT(*c) && D > 0) { X = X * 10 + (*c - '0'); c++; D--; }
 
-// originally inspired by Simon Urbanek fasttime package
-SEXP parse_dt(SEXP str, SEXP ord) {
 
-  if (TYPEOF(str) != STRSXP) Rf_error("Date-time must be a character vector");
-  if ((TYPEOF(ord) != STRSXP) || (LENGTH(ord) > 1))
-    Rf_error("Orders vector must be a character vector of length 1");
+SEXP parse_dt(SEXP str, SEXP ord, SEXP formats) {
+  // ord: formats (as in strptime) or orders (as in parse_date_time)
+  // formats: TRUE if ord are formats as in strptime, otherwise they are orders.
+
+  if ( !isString(str) ) Rf_error("Date-time must be a character vector");
+  if ( !isString(ord) || (LENGTH(ord) > 1))
+    Rf_error("Format argument must be a character vector of length 1");
 
   int i, n = LENGTH(str);
-  const char *O = CHAR(STRING_ELT(ord, i));
+  int *fmt = LOGICAL(formats);
+  const char *O = CHAR(STRING_ELT(ord, 0));
 
   SEXP res;
   double *data;
@@ -33,102 +36,114 @@ SEXP parse_dt(SEXP str, SEXP ord) {
   res = Rf_allocVector(REALSXP, n);
   data = REAL(res);
 
+
   for (i = 0; i < n; i++) {
 
     const char *c = CHAR(STRING_ELT(str, i));
     const char *o = O;
 
     int y = 0, m = 0, d = 0, H = 0, M = 0 , S = 0;
-    int succeed = 1, leap = 0;
+    int succeed = 1, leap = 0, D;
     double secs = 0.0; // main accumulator
 
-    // skip all leading non-digits
-    while (*c && !DIGIT(*c)) c++;
+    while( *o && succeed ){
 
-    if (DIGIT(*c)) {
-
-      while( *o && succeed ){
-        switch( *o ) {
-        case 'Y': // year in yyyy format
-          PARSENUM(y)
-          break;
-        case 'y': // year in yy format
-          PARSENUM(y)
-          // we allow yyyy years, but interpret yy as > 2000
-          if (y < 100) y += 2000;
-          break;
-        case 'm': // month
-          PARSENUM(m)
-          if (0 < m < 13) secs += sm[m];
-          else succeed = 0;
-          break;
-        case 'd': // day
-          PARSENUM(d)
-          if ( d < 32 ) secs += (d - 1) * 86400;
-          else succeed = 0;
-          break;
-        case 'H': // hour 24
-          PARSENUM(H)
-          if( H < 25 ) secs += H * 3600;
-          else  succeed = 0;
-          break;
-        case 'M': // minute
-          PARSENUM(M)
-          if ( M < 61 ) secs += M * 60;
-          else succeed = 0;
-          break;
-        case 'S':
-          PARSENUM(S)
-          // allow leap seconds
-          if ( S < 62 ){
-            secs += S;
-            // both . and , as decimal separator are allowed
-            if( *c == '.' || *c == ','){
-              double ms = 0.0, msfact = 0.1;
-              c++;
-              while (DIGIT(*c)) { ms = ms + (*c - '0')*msfact; msfact *= 0.1; c++; }
-              secs += ms;
-            }
-          } else succeed = 0;
-          break;
-        default:
-          Rf_error("Unrecognized format %c supplied", *o);
-        }
-        while (*c && !DIGIT(*c)) c++;
-        *o++;
-      }
-
-      if ( succeed ){
-
-        y -= 2000;
-        secs += y * yearlen;
-
-        // leap year every 400 years; no leap every 100 years
-        int leap4 = y % 4 == 0;
-        int skip100leap = leap4 && y % 100 == 0 && y % 400 != 0;
-
-        if ( y >= 0 ){
-          // ordinary leap days before 2000-01-01 00:00:00
-          secs += ( y / 4 ) * daylen + daylen;
-          if( y > 99 )
-            secs += (y / 400 - y / 100) * daylen;
-          // adjust if within leap year
-          if ( leap4 && m < 3 && !skip100leap )
-            secs -= daylen;
-
-        } else {
-          secs += (y / 4) * daylen;
-          if( y < -99 )
-            secs += (y / 400 - y / 100) * daylen;
-          if ( leap4 && m > 2 && !skip100leap )
-            secs += daylen;
-        }
-
-        data[i] = secs + d30;
+      if( *fmt && (*o != '%')) {
+        // when formats, non fomrating character should match exactly
+        if ( *c == *o ) { c++; o++; } else succeed = 0;
 
       } else {
-        data[i] = NA_REAL;
+
+        if ( *fmt ) o++; // sckip %
+        else while (*c && !DIGIT(*c)) c++; // skip non-digits
+
+        if ( !DIGIT(*c) ){
+          succeed = 0;
+        } else {
+          switch( *o ) {
+          case 'Y': // year in yyyy format
+            PARSENUM(y, 4);
+            break;
+          case 'y': // year in yy format
+            PARSENUM(y, 2);
+            if (y < 100) y += 2000;
+            break;
+          case 'm': // month
+            PARSENUM(m, 2);
+            if (0 < m < 13) secs += sm[m];
+            else succeed = 0;
+            break;
+          case 'd': // day
+            PARSENUM(d, 2);
+            if ( d < 32 ) secs += (d - 1) * 86400;
+            else succeed = 0;
+            break;
+          case 'H': // hour 24
+            PARSENUM(H, 2);
+            if( H < 25 ) secs += H * 3600;
+            else  succeed = 0;
+            break;
+          case 'M': // minute
+            PARSENUM(M, 2);
+            if ( M < 61 ) secs += M * 60;
+            else succeed = 0;
+            break;
+          case 'S':
+            PARSENUM(S, 2);
+            // allow leap seconds
+            if ( S < 62 ){
+              secs += S;
+              // both . and , as decimal separator are allowed
+              if( *c == '.' || *c == ','){
+                double ms = 0.0, msfact = 0.1;
+                c++;
+                while (DIGIT(*c)) { ms = ms + (*c - '0')*msfact; msfact *= 0.1; c++; }
+                secs += ms;
+              }
+            } else succeed = 0;
+            break;
+          default:
+            Rf_error("Unrecognized format %c supplied", *o);
+          }
+          *o++;
+        }
       }
+    }
+
+    // if at least one not finished, consider as a failure
+    // fixme: allow truncations
+    if ( *c || *o ) succeed = 0;
+
+    // process year
+    if ( succeed ){
+
+      y -= 2000;
+      secs += y * yearlen;
+
+      // leap year every 400 years; no leap every 100 years
+      int leap4 = y % 4 == 0;
+      int skip100leap = leap4 && y % 100 == 0 && y % 400 != 0;
+
+      if ( y >= 0 ){
+        // ordinary leap days before 2000-01-01 00:00:00
+        secs += ( y / 4 ) * daylen + daylen;
+        if( y > 99 )
+          secs += (y / 400 - y / 100) * daylen;
+        // adjust if within leap year
+        if ( leap4 && m < 3 && !skip100leap )
+          secs -= daylen;
+
+      } else {
+        secs += (y / 4) * daylen;
+        if( y < -99 )
+          secs += (y / 400 - y / 100) * daylen;
+        if ( leap4 && m > 2 && !skip100leap )
+          secs += daylen;
+      }
+
+      data[i] = secs + d30;
+    } else {
+      data[i] = NA_REAL;
     }
   }
   return res;
