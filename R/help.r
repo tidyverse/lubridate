74aff1504d75dda57a2a22c629e33ead76deb901
Vitalie Spinu <spinuvit@gmail.com>
2015-01-10 20:11:40 +0100

    Remove `plyr` unnecessary dependency

--- R/help.r
+++ R/help.r
@@ -163,7 +163,7 @@
 #' @references Garrett Grolemund, Hadley Wickham (2011). Dates and Times Made
 #'   Easy with lubridate. Journal of Statistical Software, 40(3), 1-25. 
 #'   \url{http://www.jstatsoft.org/v40/i03/}.
-#' @import plyr stringr
+#' @import stringr
 #' @docType package
 #' @name lubridate
 #' @aliases lubridate lubridate-package
