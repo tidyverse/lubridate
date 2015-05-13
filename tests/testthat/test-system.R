f9761ce5655f35f4512509eb168c2d98ad935a49
Vitalie Spinu <spinuvit@gmail.com>
2015-05-13 23:08:00 +0200

    Typo

--- tests/testthat/test-system.R
+++ tests/testthat/test-system.R
@@ -1,10 +1,10 @@
 context("Sytem")
 
 
-test_that("Rscripts runs correctly", {
-            skip_on_cran()
-            if(nzchar(Sys.getenv("TRAVIS")))
-              skip("Running  on Travis")
-            expect_equal(0L, system("Rscript -e \"lubridate::round_date(Sys.time(),'minute')\"",
-                                    ignore.stderr = T, ignore.stdout = T))
-          })
+test_that("Rscript runs correctly", {
+  skip_on_cran()
+  if(nzchar(Sys.getenv("TRAVIS")))
+    skip("Running  on Travis")
+  expect_equal(0L, system("Rscript -e \"lubridate::round_date(Sys.time(),'minute')\"",
+                          ignore.stderr = T, ignore.stdout = T))
+})
