a8e69450a111c6819232bc4c383355bff6887fb2
Vitalie Spinu <spinuvit@gmail.com>
2015-12-25 11:20:27 +0100

    More improvements of `week` docs

--- R/accessors-week.r
+++ R/accessors-week.r
@@ -3,20 +3,19 @@ NULL
 
 #' Get/set weeks component of a date-time.
 #'
-#' Date-time must be a POSIXct, POSIXlt, Date, chron, yearmon, yearqtr, zoo,
-#' zooreg, timeDate, xts, its, ti, jul, timeSeries, and fts objects.
-#'
 #' \code{week} returns the number of complete seven day periods that have
-#' occured between the date and January 1st, plus one.
-#'
+#' occured between the date and January 1st, plus one.\cr
 #' \code{isoweek} returns the week as it would appear in the ISO 8601 system,
 #' which uses a reoccuring leap week.
 #'
-#' @param x a date-time object   
+#' @param x a date-time object. Must be a POSIXct, POSIXlt, Date, chron,
+#'   yearmon, yearqtr, zoo, zooreg, timeDate, xts, its, ti, jul, timeSeries, or
+#'   fts object.
 #' @param value a numeric object
 #' @return the weeks element of x as an integer number
 #' @keywords utilities manip chron
 #' @references \url{http://en.wikipedia.org/wiki/ISO_week_date}
+#' @seealso \code{\link{isoyear}}
 #' @examples
 #' x <- ymd("2012-03-26")
 #' week(x) 
