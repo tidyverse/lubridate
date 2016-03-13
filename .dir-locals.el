17fdff6dcc55bdf4ec07c11ebc058c393729b100
Vitalie Spinu <spinuvit@gmail.com>
2016-03-13 11:25:24 +0100

    Update .dir-locals to remove white spaces on save

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,12 @@
 ;;; Directory Local Variables for Emacs
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((ess-mode
-  (indent-tabs-mode . nil)
-  (tab-width . 2)
-  (fill-column . 80)
+((nil
+  (require-final-newline . t)
+  ;; not tabs in code
+  (indent-tabs-mode)
+  ;; remove trailing whitespace
+  (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)))
+ (ess-mode
   (ess-indent-level . 2)))
+
