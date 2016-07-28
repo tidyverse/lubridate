2c1d47daf46613ee42c9d63637397ecabb7920bc
hadley <h.wickham@gmail.com>
2016-07-28 15:54:35 -0500

    Usage for POSIX testers

--- R/POSIXt.r
+++ R/POSIXt.r
@@ -5,7 +5,6 @@ setOldClass("POSIXlt")
 #' Is x a POSIXct or POSIXlt object?
 #'
 #' @export
-#' @aliases is.POSIXt is.POSIXlt is.POSIXct
 #' @param x an R object
 #' @return TRUE if x is a POSIXct or POSIXlt object, FALSE otherwise.
 #' @seealso \code{\link{is.instant}}, \code{\link{is.timespan}}, \code{\link{is.Date}}
@@ -14,8 +13,12 @@ setOldClass("POSIXlt")
 #' is.POSIXt(as.Date("2009-08-03")) # FALSE
 #' is.POSIXt(as.POSIXct("2009-08-03")) # TRUE
 is.POSIXt <- function(x) inherits(x, "POSIXt")
+
+#' @rdname is.POSIXt
 #' @export
 is.POSIXlt <- function(x) inherits(x, "POSIXlt")
+
+#' @rdname is.POSIXt
 #' @export
 is.POSIXct <- function(x) inherits(x, "POSIXct")
 
