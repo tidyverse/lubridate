a55725c1dfd5c6f1652892d9c6b6a762124fe5b1
Vitalie Spinu <spinuvit@gmail.com>
2017-10-31 00:02:22 +0100

    [Fix #575] Improve guessing of zoneinfo directory

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,25 @@
 
-## Set zoneinfo location for CCTZ on windows. Copied from RcppCCTZ package.
 .onAttach <- function(libname, pkgname) {
-    if (Sys.info()[["sysname"]] == "Windows") {
-        ## if (interactive()) packageStartupMessage("RcppCCTZ setting zoneinfo on Windows")
-        Sys.setenv("TZDIR" = file.path(R.home(), "share", "zoneinfo"))
+  ## CCTZ needs zoneinfo. On windows we set it to R's own zoneinfo. On unix like
+  ## it's in "/usr/share/zoneinfo" where CCTZ looks by default. On some systems
+  ## (solaris?) it might be in a different location. So, help ourselves by
+  ## setting the TZDIR env var.
+  if (Sys.getenv("TZDIR") == "") {
+    ## adapted from OlsonNames function
+    tzdir <-
+      if (.Platform$OS.type == "windows") {
+        file.path(R.home("share"), "zoneinfo")
+      } else if (!file.exists("/usr/share/zoneinfo")) {
+        tzdirs <- c(file.path(R.home("share"), "zoneinfo"),
+                    "/usr/share/lib/zoneinfo", "/usr/lib/zoneinfo",
+                    "/usr/local/etc/zoneinfo", "/etc/zoneinfo",
+                    "/usr/etc/zoneinfo")
+        tzdirs <- tzdirs[file.exists(tzdirs)]
+        if (length(tzdirs)) tzdirs[[1]]
+        else NULL
+      }
+    if (!is.null(tzdir)) {
+      Sys.setenv("TZDIR", tzdir)
     }
+  }
 }
