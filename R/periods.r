18ca939fe72920cab80e526d377a932825fd9323
Robert Karlsson <robert.karlsson@ki.se>
2018-11-21 11:23:47 +0100

    Fix typo in period docs

--- R/periods.r
+++ R/periods.r
@@ -298,14 +298,10 @@ setMethod("$<-", signature(x = "Period"), function(x, name, value) {
 #' to and subtracted to date-times to create a user interface similar to object
 #' oriented programming.
 #'
-#' Note: Arithmetic with periods can results in undefined behavior when
-#' non-existent dates are involved (such as February 29th). Please see
-#' [Period-class] for more details and \code{\link{\%m+\%}} and
-#' [add_with_rollback()] for alternative operations. Note: Arithmetic with
-#' periods can results in undefined behavior when non-existent dates are
-#' involved (such as February 29th in non-leap years). Please see [Period-class]
-#' for more details and \code{\link{\%m+\%}} and [add_with_rollback()] for
-#' alternative operations.
+#' Note: Arithmetic with periods can result in undefined behavior when
+#' non-existent dates are involved (such as February 29th in non-leap years).
+#' Please see [Period-class] for more details and \code{\link{\%m+\%}} and
+#' [add_with_rollback()] for alternative operations.
 #'
 #' @name period
 #' @aliases periods
