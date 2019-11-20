1659b91c88855ac6f156e9a904b088fd1697ce35
Hadley Wickham <h.wickham@gmail.com>
2019-11-20 09:06:36 -0600

    Restyle

--- R/ops-integer-division.r
+++ R/ops-integer-division.r
@@ -2,13 +2,15 @@
 NULL
 
 integer_divide_spans <- function(e1, e2) {
-  trunc(e1/e2)
+  trunc(e1 / e2)
 }
 
 #' @export
 setMethod("%/%", signature(e1 = "Timespan", e2 = "Timespan"),
-  function(e1, e2) integer_divide_spans(e1, e2))
+  function(e1, e2) integer_divide_spans(e1, e2)
+)
 
 #' @export
 setMethod("%/%", signature(e1 = "difftime", e2 = "Timespan"),
-          function(e1, e2) integer_divide_spans(e1, e2))
+  function(e1, e2) integer_divide_spans(e1, e2)
+)
