b54ce89476f3deb04087e7b9648fa60a9d60b55b
Vitalie Spinu <spinuvit@gmail.com>
2021-02-26 09:52:56 +0100

    Run revdeps

--- revdep/check.R
+++ revdep/check.R
@@ -3,12 +3,10 @@ devtools::install_github("r-lib/revdepcheck")
 
 library(revdepcheck)
 revdep_reset()
-revdep_check(num_workers = 8)
+revdep_check(num_workers = 7)
 revdep_summary()
 revdep_todo()
 revdep_add_broken()
 revdep_rm(packages = "xROI")
-revdep_reset()
-revdep_check()
 
 revdep_maintainers()
