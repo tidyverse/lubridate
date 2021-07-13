1945f06ae18f075c6635f41d4c82b06c70cae28f
David Fong <vkelim@bigpond.com>
2021-07-13 18:39:11 +1000

    `quiet` parameter for `stamp_date` and `stamp_time`
    pass `quiet` parameter to `stamp`

--- R/stamp.r
+++ R/stamp.r
@@ -196,14 +196,15 @@ stamp <- function(x, orders = lubridate_formats,
 
 ##' @rdname stamp
 ##' @export
-stamp_date <- function(x, locale = Sys.getlocale("LC_TIME"))
+stamp_date <- function(x, locale = Sys.getlocale("LC_TIME"), quiet = FALSE)
   stamp(x, orders = c("ymd", "dmy", "mdy", "ydm", "dym", "myd", "my", "ym", "md", "dm", "m", "d", "y"),
-        locale = locale)
+        locale = locale, quiet = quiet)
 
 ##' @rdname stamp
 ##' @export
-stamp_time <- function(x, locale = Sys.getlocale("LC_TIME"))
-  stamp(x, orders = c("hms", "hm", "ms", "h", "m", "s"), locale = locale)
+stamp_time <- function(x, locale = Sys.getlocale("LC_TIME"), quiet = FALSE)
+  stamp(x, orders = c("hms", "hm", "ms", "h", "m", "s"),
+        locale = locale, quiet = quiet)
 
 
 lubridate_formats <- local({
