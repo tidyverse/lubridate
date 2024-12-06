369076612c43edd2c4929fb03f85278cf9d01b52
Harshini <61635908+hersheyk@users.noreply.github.com>
2024-12-06 14:27:33 -0800

    Update accessors-term.r
    switched to term and type from type and format

--- R/accessors-term.r
+++ R/accessors-term.r
@@ -1,4 +1,20 @@
-term <- function(x, type = "quarter", fiscal, with_year, format, holidays = NULL){
+#' Get the fiscal term of a date-time
+#'
+#' Quarters divide the year into fourths. Trimesters divide the year into thirds. Semesters divide the year into halfs.
+#'
+#' @param x a date-time object of class POSIXct, POSIXlt, Date, chron, yearmon, yearqtr,
+#'   zoo, zooreg, timeDate, xts, its, ti, jul, timeSeries, fts or anything else that can
+#'   be converted with as.POSIXlt
+#' @param term the way you are dividing the year, be it semesters, trimestes, or quarters.
+#' @param type the format to be returned for the quarter. Can be one one of "quarter" -
+#'   return numeric quarter (default), "year.quarter" return the ending year and quarter
+#'   as a number of the form year.quarter, "date_first" or "date_last" - return the date
+#'   at the quarter's start or end, "year_start/end" - return a full description of the
+#'   quarter as a string which includes the start and end of the year
+#'   (ex. "2020/21 Q1").
+#' @param fiscal_start numeric indicating the starting month of a fiscal year.
+
+term <- function(x, term = "quarter", type = "quarter", fiscal_start = 1, holidays = NULL){
   if (length(fiscal_start) > 1) {
     stop("`fiscal_start` must be a singleton", call. = FALSE)
   }
@@ -18,11 +34,8 @@ term <- function(x, type = "quarter", fiscal, with_year, format, holidays = NULL
   divisions <- rep(1:num_divisions, each = 12 / num_divisions)
   current_division <- divisions[s]
 
-  if (is.logical(format)) {
-    format <- if (format) "year.quarter" else "quarter"
-  }
-  if (with_year == TRUE) {
-    format <- "year.quarter"
+  if (is.logical(type)) {
+    type <- if (type) "year.quarter" else "quarter"
   }
   
   
