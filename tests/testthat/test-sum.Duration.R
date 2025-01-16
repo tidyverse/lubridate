0cdac82a138b9e944c0f7c4d6cca9c0d2fa705e4
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-15 18:31:18 -0800

    more

--- tests/testthat/test-sum.Duration.R
+++ tests/testthat/test-sum.Duration.R
@@ -1,6 +1,6 @@
 test_that("`sum()` works for Durations", {
 
-  x = duration(c(1,2))
+  x = lubridate::duration(1:2)
   expected = x[1] + x[2]
   expect_equal(expected, sum(x))
   expect_s4_class(sum(x), "Duration")
