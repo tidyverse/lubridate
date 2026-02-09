255358c560af1e2bba9759d8885d6ae45e124976
Vitalie Spinu <vitalie@Vitalies-MacBook-Pro.local>
2026-02-09 10:46:05 +0100

    Remove trailing reference to GNU licence (fixes #1207)

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,6 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil
-  (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
   (bug-reference-url-format . "https://github.com/tidyverse/lubridate/issues/%s")
   (require-final-newline . t)
   ;; not tabs in code
